a79a088fb0c89bcc21c39c27810aab4ec1324009
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2023-12-01 16:04:28 +0100

    feat: resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+-- For support join my discord: https://discord.gg/Z9Mxu72zZ6
+
+author "Andyyy7666"
+description "Casino stuff"
+version "1.0.0"
+
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+files {
+    "data/**",
+    "modules/**/client.lua"
+}
+
+shared_scripts {
+    "@ox_lib/init.lua",
+    "shared/**"
+}
+
+client_scripts {
+    "client/**"
+}
+server_scripts {
+    "server/**"
+}
