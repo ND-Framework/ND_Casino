1e11e7e3ce53116902bfff62b36cd63301de2b9e
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2023-12-01 17:33:58 +0100

    feat: casino valet

---
+++ client/valet.lua
@@ -0,0 +1,106 @@
+local createAiPed = require "modules.peds.client"
+local createProp = require "modules.props.client"
+
+local props = {
+    {
+        model = `vw_prop_vw_valet_01a`,
+        location = vec4(925.893, 51.215, 80.106, 58.0)
+    },
+    {
+        model = `prop_notepad_02`,
+        location = vec4(925.89, 51.24, 81.105, 64.0)
+    },
+    {
+        model = `prop_pencil_01`,
+        location = vec4(925.89, 51.24, 81.113, 280.0)
+    }
+}
+
+local peds = {
+    {
+        model = `S_M_Y_Casino_01`,
+        coords = vec4(926.31, 50.96, 81.10, 58.0),
+        distance = 60.0,
+        anim = {
+            dict = "anim@amb@casino@valet_scenario@pose_a@",
+            clip = "base_a_m_y_vinewood_01"
+        },
+        clothing = {
+            mask = {drawable = 1, texture = 0},
+            bag = {drawable = 0, texture = 0},
+            torso2 = {drawable = 1, texture = 1},
+            badge = {drawable = 0, texture = 0},
+            shoes = {drawable = 1, texture = 0},
+            undershirt = {drawable = 3, texture = 0},
+            leg = {drawable = 0, texture = 0},
+            accessory = {drawable = 2, texture = 0},
+            torso = {drawable = 1, texture = 4},
+            kevlar = {drawable = 0, texture = 0},
+            face = {drawable = 5, texture = 0},
+            hair = {drawable = 5, texture = 0},
+            hat = {drawable = -1, texture = -1},
+            glasses = {drawable = -1, texture = -1},
+            watch = {drawable = -1, texture = -1},
+            ears = {drawable = -1, texture = -1},
+            bracelets = {drawable = -1, texture = -1}
+        },
+        options = {
+            {
+                name = "nd_casino:valet",
+                icon = "fa-solid fa-square-parking",
+                label = "Park vehicle",
+                distance = 2.0,
+                onSelect = function(data)
+                    local vehicle = Bridge.getNearestValetVehicle(cache.ped)
+                    if not vehicle then return end
+                    for i=0, 5 do SetVehicleDoorShut(vehicle, i, false) end
+                    
+                    local netId = NetworkGetNetworkIdFromEntity(vehicle)
+                    TriggerServerEvent("ND_Casino:parkVehicleValet", netId)
+                end
+            },
+            {
+                name = "nd_casino:valet",
+                icon = "fa-solid fa-car",
+                label = "Bring vehicle",
+                distance = 2.0,
+                onSelect = function(data)
+                    local vehicle = Bridge.getNearestValetVehicle(cache.ped)
+                    if not vehicle then return end
+
+                    local netId = NetworkGetNetworkIdFromEntity(vehicle)
+                    TriggerServerEvent("ND_Casino:bringValet", netId)
+                end
+            }
+        },
+    }
+}
+
+for i=1, #props do
+    createProp(props[i])
+end
+
+for i=1, #peds do
+    createAiPed(peds[i])
+end
+
+
+
+-- local parkings = {}
+-- RegisterCommand("set-parking", function(source, args, rawCommand)
+--     local coords = GetEntityCoords(cache.vehicle)
+--     local heading = GetEntityHeading(cache.vehicle)
+--     local park = vec4(coords.x, coords.y, coords.z, heading)
+--     local index = #parkings+1
+--     parkings[index] = park
+--     print("Created parking", index, park)
+-- end, false)
+
+-- RegisterCommand("get-parkings", function(source, args, rawCommand)
+--     local info = ""
+--     for i=1, #parkings do
+--         local park = parkings[i]
+--         info = ("%s\nvec4(%.2f, %.2f, %.2f, %.2f),"):format(info, park.x, park.y, park.z, park.w)
+--     end
+--     lib.setClipboard(info)
+-- end, false)
---
+++ server/valet.lua
@@ -0,0 +1,102 @@
+local parkings = require "data.parking"
+local parkedVehicles = {}
+
+local function isParkingAvailable(vehicles, spot)
+    for i=1, #vehicles do
+        local veh = vehicles[i]
+        local coords = GetEntityCoords(veh)
+        if #(coords-spot.xyz) < 2 then
+            return
+        end
+    end
+    return true
+end
+
+local function getParking(spots)
+    local vehicles = GetAllVehicles()
+    return lib.waitFor(function()
+        local spot = spots[math.random(1, #spots)]
+        if isParkingAvailable(vehicles, spot) then
+            return spot
+        end
+    end)
+end
+
+local function getVehicleFromNetId(netId)
+    return lib.waitFor(function()
+        local entity = NetworkGetEntityFromNetworkId(netId)
+        if DoesEntityExist(entity) then return entity end
+    end)
+end
+
+RegisterNetEvent("ND_Casino:parkVehicleValet", function(netId)
+    local src = source
+    local vehicle = getVehicleFromNetId(netId)
+    if not vehicle then
+        return TriggerClientEvent("ox_lib:notify", src, {
+            title = "Vehicle not found!",
+            description = "You don't have keys to any nearby vehicle",
+            type = "error"
+        })
+    end
+
+    local parking = getParking(parkings.carpark)
+    if not parking then
+        return TriggerClientEvent("ox_lib:notify", src, {
+            title = "Parking not found!",
+            description = "We don't have any parking spots left",
+            type = "error"
+        })
+    end
+
+    if parkedVehicles[src] then
+        if getVehicleFromNetId(parkedVehicles[src]) == vehicle then
+            return TriggerClientEvent("ox_lib:notify", src, {
+                title = "Vehicle already parked!",
+                description = "We've already parked this vehicle for you",
+                type = "error"
+            })
+        end
+    end
+
+    SetEntityCoords(vehicle, parking.x, parking.y, parking.z)
+    SetEntityHeading(vehicle, parking.w)
+    parkedVehicles[src] = netId
+    TriggerClientEvent("ox_lib:notify", src, {
+        title = "Vehicle parked!",
+        description = "We've taken care of your vehicle and parked it",
+        type = "success"
+    })
+end)
+
+RegisterNetEvent("ND_Casino:bringValet", function(netId)
+    local src = source
+    if not parkedVehicles[src] then return end
+
+    local vehicle = getVehicleFromNetId(netId)
+    if not vehicle then
+        return TriggerClientEvent("ox_lib:notify", src, {
+            title = "Vehicle not found!",
+            description = "We couldn't find your vehicle",
+            type = "error"
+        })
+    end
+
+    local parking = getParking(parkings.valet)
+    if not parking then
+        return TriggerClientEvent("ox_lib:notify", src, {
+            title = "Vehicle not found!",
+            description = "We couldn't find your vehicle",
+            type = "error"
+        })
+    end
+
+    SetEntityCoords(vehicle, parking.x, parking.y, parking.z)
+    SetEntityHeading(vehicle, parking.w)
+    parkedVehicles[src] = nil
+    TriggerClientEvent("ox_lib:notify", src, {
+        title = "Vehicle brought back!",
+        description = "We've brought your vehicle back to you",
+        type = "success"
+    })
+end)
